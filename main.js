0432f8bf0e50c09942752ff5476144600862eff4
udommiracle2 <udommiracle2@gmail.com>
2025-11-25 19:09:44 +0100

    12th commit

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ btns.forEach(btn => {
     btn.addEventListener( "click", () => {
     btn.classList.toggle("fa-toggle-on");
     btn.classList.toggle("fa-toggle-off");
-
 });
 });
 
@@ -19,11 +18,24 @@ actives.forEach(active => {
 });
 
 
-const darkMode = document.querySelectorAll('#dark-mode');
+document.getElementById('dark-mode').onclick = () => {
+    let body = document.getElementById('light');
+    let icon = document.getElementById('dark-mode');
+    if (body.style.background === 'white') {
+        body.style.background = 'hsl(225, 78%, 16%)';
+        body.style.color = 'white';
+        body.style.transition = '0.5s';
 
-darkMode.forEach( darkModes => {
-    darkModes.addEventListener("click", () => {
-        darkModes.classList.toggle("fa-moon");
-        darkModes.classList.toggle("fa-sun");
-    })
-});
+        icon
+        
+    }
+    else {
+        body.style.background = 'white';
+        body.style.color = 'hsl(225, 78%, 16%)';
+        document.getElementById('dark-mode').toggle('fa-sun');
+    }
+    
+
+    document.getElementById('sun').innerHTML = '<i class="fa- regular fa-sun"></i>'
+    
+}

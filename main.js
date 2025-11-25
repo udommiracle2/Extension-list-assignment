a9bdd596370de3f086d6f41a28949674dd4b7553
udommiracle2 <udommiracle2@gmail.com>
2025-11-25 19:19:24 +0100

    13th commit

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const btns = document.querySelectorAll('.fa-solid');
 
 btns.forEach(btn => {
-    btn.addEventListener( "click", () => {
-    btn.classList.toggle("fa-toggle-on");
-    btn.classList.toggle("fa-toggle-off");
-});
+    btn.addEventListener("click", () => {
+        btn.classList.toggle("fa-toggle-on");
+        btn.classList.toggle("fa-toggle-off");
+    });
 });
 
 
 const actives = document.querySelectorAll('.btn');
 
 actives.forEach(active => {
-    active.addEventListener("click", () =>{
+    active.addEventListener("click", () => {
         document.querySelector('.active')?.classList.remove('active');
         active.classList.add('active');
     });
@@ -20,22 +20,22 @@ actives.forEach(active => {
 
 document.getElementById('dark-mode').onclick = () => {
     let body = document.getElementById('light');
-    let icon = document.getElementById('dark-mode');
+    let icon = document.getElementById('sun');
     if (body.style.background === 'white') {
         body.style.background = 'hsl(225, 78%, 16%)';
         body.style.color = 'white';
         body.style.transition = '0.5s';
 
-        icon
-        
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+
     }
     else {
         body.style.background = 'white';
         body.style.color = 'hsl(225, 78%, 16%)';
-        document.getElementById('dark-mode').toggle('fa-sun');
-    }
-    
 
-    document.getElementById('sun').innerHTML = '<i class="fa- regular fa-sun"></i>'
-    
+        icon.classList.add('fa-sun');
+        icon.classList.remove('fa-moon');
+    }
+
 }
